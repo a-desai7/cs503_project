dc84b74819263891bdbd93f3b3e0444dd72a1ec0
Csaba Beleznai <csaba.beleznai@epfl.ch>
2025-05-21 01:25:23 +0200

    moved test augmentation to configs

--- configs/night_base.py
+++ configs/night_base.py
@@ -97,7 +97,8 @@ test_pipeline = [
     dict(
         type="MultiScaleFlipAug",
         img_scale=(1024, 512),
-        flip=False,
+        img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
+        flip=True,
         transforms=[
             dict(type="Resize", keep_ratio=True),
             dict(type="RandomFlip"),
--- custom-tools/test.py
+++ custom-tools/test.py
@@ -142,12 +142,15 @@ def main():
     # set cudnn_benchmark
     if cfg.get('cudnn_benchmark', False):
         torch.backends.cudnn.benchmark = True
-    if args.aug_test:
-        # hard code index
-        cfg.data.test.pipeline[1].img_ratios = [
-            0.5, 0.75, 1.0, 1.25, 1.5, 1.75
-        ]
-        cfg.data.test.pipeline[1].flip = True
+        
+    # This was implemented in the config
+    # if args.aug_test:
+    #     # hard code index
+    #     cfg.data.test.pipeline[1].img_ratios = [
+    #         0.5, 0.75, 1.0, 1.25, 1.5, 1.75
+    #     ]
+    #     cfg.data.test.pipeline[1].flip = True
+    
     cfg.model.pretrained = None
     cfg.data.test.test_mode = True
 
